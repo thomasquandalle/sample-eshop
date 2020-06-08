68fd44168029085fe5fedffee264427f25120446
Thomas Quandalle <thomas.quandalle@gmail.com>
2020-06-08 21:52:04 +0200

    Add a possibility to change product qty in cart overlay

--- src/components/UI/Cart/CartOverlay.jsx
+++ src/components/UI/Cart/CartOverlay.jsx
@@ -1,7 +1,16 @@
+/*
+Overlay containing the list of products currently in the cart
+From this overlay, you can increase or decrease the quantity of the product
+You can also delete the product from your cart
+props:{
+	open: (Bool) The visibility of the overlay
+}
+ */
+
 import React from "react";
 import {Overlay} from "../../genericComponents/Overlay";
 import { connect } from "react-redux";
-import {removeAllProduct} from "../../../redux/actions/cartActions";
+import {addProduct, deleteProduct, removeAllProduct} from "../../../redux/actions/cartActions";
 
 const styles = {
 	ul: {
@@ -30,6 +39,18 @@ const styles = {
 	},
 	qtyP: {
 		margin: 0
+	},
+	buttonContainer:{
+		display: "flex",
+		flexDirection: "column",
+	},
+	qtyButton: {
+		border: 0,
+		margin: 1,
+		backgroundColor: "rgb(95,180,174)",
+		color: "rgb(255,255,255)",
+		boxShadow: "none",
+		borderRadius: "10px",
 	}
 };
 
@@ -40,9 +61,19 @@ const CartOverlay = (props) =>(
 			<li key = {product.id} style = {styles.li}>
 				<p style = {styles.titleP}>{product.title[0].toUpperCase() + product.title.slice(1)}</p>
 				<p style = {styles.qtyP}> {product.qty}</p>
+				<div style = {styles.buttonContainer}>
+					<button
+						onClick={() => props.addProduct(product.id)}
+						style = {styles.qtyButton}
+					>+</button>
+					<button
+						onClick={() => props.removeProduct(product.id)}
+						style = {styles.qtyButton}
+					>-</button>
+				</div>
 				<button
 					style = {styles.button}
-					onClick={() => props.removeProduct(product.id)}
+					onClick={() => props.deleteProduct(product.id)}
 				>Delete</button>
 			</li>
 		))}
@@ -66,7 +97,9 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
 	return{
-		removeProduct: id => dispatch(removeAllProduct(id))
+		deleteProduct: id => dispatch(removeAllProduct(id)),
+		addProduct: id => dispatch(addProduct(id)),
+		removeProduct: id => dispatch(deleteProduct(id))
 	}
 };
 
